ac3712e6c436cca44b53d44868da19f5e7bf894e
Thanasis Nikolopoulos <81475280+thanasisnik@users.noreply.github.com>
2025-05-08 23:08:11 +0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <img
+        <Image
           className="dark:invert"
-          src="{`${process.env.NEXT_PUBLIC_BASE_PATH || ''}/next.svg"
+          src="{`${basePath}/next.svg`}"
           alt="Next.js logo"
           width={180}
           height={38}
@@ -61,7 +61,7 @@ export default function Home() {
         >
           <img
             aria-hidden
-            src="{`${process.env.NEXT_PUBLIC_BASE_PATH || ''}/file.svg"
+            src="{`${basePath}/file.svg`}"
             alt="File icon"
             width={16}
             height={16}
