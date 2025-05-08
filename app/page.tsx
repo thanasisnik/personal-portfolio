f0f0b2b390e1add1b422c9afbcbb0d793b71eb6a
Thanasis Nikolopoulos <81475280+thanasisnik@users.noreply.github.com>
2025-05-08 22:57:40 +0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,13 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
+        <img
           className="dark:invert"
           src="{{NEXT_PUBLIC_BASE_PATH}}/next.svg"
           alt="Next.js logo"
           width={180}
           height={38}
-          priority
+          
         />
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
@@ -59,7 +59,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
+          <img
             aria-hidden
             src="{{NEXT_PUBLIC_BASE_PATH}}/file.svg"
             alt="File icon"
@@ -74,7 +74,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
+          <img
             aria-hidden
             src="{{NEXT_PUBLIC_BASE_PATH}}/window.svg"
             alt="Window icon"
@@ -89,7 +89,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
+          <img
             aria-hidden
             src="{{NEXT_PUBLIC_BASE_PATH}}/globe.svg"
             alt="Globe icon"
