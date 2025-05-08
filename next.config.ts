c7012365dda3c733f16fb9954df3e0c7c1f91363
Thanasis Nikolopoulos <81475280+thanasisnik@users.noreply.github.com>
2025-05-08 21:44:46 +0300

    nextConfig update

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
-  images: {unoptimized: true},
+  images: {
+    unoptimized: true,
+    loader: 'default',
+  },
   basePath: '/personal-portfolio',
   assetPrefix: '/personal-portfolio/',
 };
